d51ec946453d2bbb93f380b09e01d54c81fb8804
Matt Weeks <m_weeks@hotmail.co.uk>
2020-05-15 00:48:51 +0100

    Terraform part 1.

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,7 +1,24 @@
-# TODO: Designate a cloud provider, region, and credentials
+provider "aws" {
+  region                  = "us-east-2"
+  shared_credentials_file = "/Users/MaBP/.aws/credentials"
+}
 
+resource "aws_instance" "UdacityT2" {
+  ami           = "ami-07c1207a9d40bc3bd"
+  count         = "4"
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-623ad209"
+  tags = {
+    Name = "Udacity T2"
+  }
+}
 
-# TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
-
-
-# TODO: provision 2 m4.large EC2 instances named Udacity M4
+# resource "aws_instance" "UdacityM4" {
+#   ami           = "ami-07c1207a9d40bc3bd"
+#   count         = "2"
+#   instance_type = "m4.large"
+#   subnet_id     = "subnet-623ad209"
+#   tags = {
+#     Name = "Udacity M4"
+#   }
+# }
